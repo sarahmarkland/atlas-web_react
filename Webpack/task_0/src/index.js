ac8d3a075fa073bf2cebd29d0af8d9fa14e9d808
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-04-30 14:21:47 -0500

    task 0v2 update 3 paragraphs in index.js to what the task actually asked for. thanks.

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,8 +1,6 @@
 // src/index.js
 import $ from 'jquery';
 
-const paragraph1 = $('<p>').text('Paragraph uno');
-const paragraph2 = $('<p>').text('Paragraph deux');
-const paragraph3 = $('<p>').text('Paragraph three');
-
-$('body').append(paragraph1, paragraph2, paragraph3);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
