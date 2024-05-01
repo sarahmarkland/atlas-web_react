2e351c39f1b29adc3ec68ae76fe6ab10f5850d9d
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-05-01 13:57:07 -0500

    task 1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,20 @@
+// src/index.js
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count">0</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+$(document).ready(function() {
+  let count = 0;
+
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
+
+  $('button').on('click', _.debounce(updateCounter, 500));
+});
