37853ad51a5b081a4939cd6fe4c9b3dcdff55688
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-05-02 13:52:40 -0500

    task 2;aslkdjflask

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.jpg/,
+        type: 'asset/resource'
+      },
+    ],
+  },
+};
