9d88fb276a2f03ed5121db8f2304473a4f675a16
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-05-05 17:36:11 -0500

    task 0 dunzo

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './holbie_logo.jpg';
 import './App.css';
 
 function App() {
@@ -6,18 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>School dashboard</h1>
       </header>
+      <hr></hr>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+      <hr></hr>
+      <footer className="App-footer">
+        <p>Copyright 2024 - Holberton School</p>
+      </footer>
     </div>
   );
 }
