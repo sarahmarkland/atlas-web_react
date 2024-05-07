e3e0fa5ddde7f65241e45e643525cb47a97d01fc
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-05-07 12:29:27 -0500

    task 8

---
+++ react_intro/task_5/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+};
