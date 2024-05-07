10426e7d9abeb26a0d527db5a6d2393c9aed910e
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-05-07 12:36:09 -0500

    task_5 add image-webpack-loader to webpack config

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -17,8 +17,8 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        test: /\.jpg/,
+        use: ['file-loader', 'image-webpack-loader']
       },
     ],
   },
