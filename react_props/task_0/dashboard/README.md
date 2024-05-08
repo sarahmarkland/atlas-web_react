04b29f9387a8f76463dd2eb59500403c72be9371
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-05-08 12:50:13 -0500

    task 0 props complete

---
+++ react_props/task_0/dashboard/README.md
@@ -0,0 +1,1 @@
+readme for dashboard in task_0 react_props
---
+++ react_props/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  }
+  );
+  it('should render without crashing', () => {
+    // const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
+
+//   it('should render a div with the class App-header', () => {
+//     const wrapper = shallow(<App />);
+//     expect(wrapper.find('.App-header').exists()).toBe(true);
+//   });
+
+//   it('should render a div with the class App-body', () => {
+//     const wrapper = shallow(<App />);
+//     expect(wrapper.find('.App-body').exists()).toBe(true);
+//   });
+
+//   it('should render a div with the class App-footer', () => {
+//     const wrapper = shallow(<App />);
+//     expect(wrapper.find('.App-footer').exists()).toBe(true);
+//   });
+// });
