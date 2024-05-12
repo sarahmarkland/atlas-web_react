f3d8cd616fdfc62a0f356ec2b0691a9cebbdb5ea
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-05-12 11:20:19 -0500

    task 3, me thinks. BodySection component.

---
+++ React_component/task_3/dashboard/README.md
@@ -0,0 +1,9 @@
+README for TASK 3
+
+## BodySection component - Reusable comments and specialization
+```The purpose of the BodySection component is to create a reusable wrapper that encapsulates a section of content within a <div> with a specific class (bodySection). This component abstracts away the styling and structure of a section in your application, allowing you to easily reuse it throughout your project.
+
+By passing a title prop to the BodySection component, you can dynamically set the heading of each section. The component then renders this title inside an <h2> element along with any children components that are nested within it.
+
+This approach promotes code organization, maintainability, and scalability. Instead of repeating the same structure and styling for each section in your application, you can simply use the BodySection component and pass in the appropriate title and content as props, resulting in cleaner and more modular code.
+```
---
+++ React_component/task_3/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,76 @@
+// TASK 2
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Notifications.css';
+import NotificationItem from './NotificationItem';
+import { NotificationItemShape } from './NotificationItemShape';
+
+class Notifications extends Component {
+  static propTypes = {
+    displayDrawer: PropTypes.bool,
+    listNotifications: PropTypes.arrayOf(NotificationItemShape)
+  }
+
+  static defaultProps = {
+    displayDrawer: true,
+    listNotifications: [],
+  };
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  render() {
+    const { displayDrawer = true, listNotifications } = this.props;
+
+    return (
+      <>
+        <div className='notificationMenu'>
+          <div className='menuItem'>Your notifications</div>
+          {displayDrawer && (
+            <div className='Notifications'>
+              <button
+                style={{
+                  float: 'right',
+                  backgroundColor: 'transparent',
+                  border: 'none',
+                  cursor: 'pointer',
+                }}
+                aria-label='Close'
+                onClick={this.handleButtonClick}
+              >
+                x
+              </button>
+              {listNotifications.length === 0 ? (
+                <p>No new notification for now</p>
+              ) : (
+                <>
+                  <p>Here is the list of notifications</p>
+                  <ul>
+                    {listNotifications.map((notification) => (
+                      <NotificationItem
+                        key={notification.id}
+                        id={notification.id}
+                        type={notification.type}
+                        value={notification.value}
+                        html={notification.html}
+                        markAsRead={this.markAsRead}
+                      />
+                    ))}
+                  </ul>
+                </>
+              )}
+            </div>
+          )}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Notifications;
