c2c5b76f2e475589680927dfe2b0501b6432b0a2
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-05-21 17:53:13 -0500

    task 1 complete. tests passing.

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,16 +1,21 @@
-// Import JSON data
-import{ normalize, schema } from 'normalizr';
-import * as notificationsData from '../../../notifications.json';
+// Function to filter notifications to return only those where
+// author.id matches given userId, and then maps those notifications to
+// return their context. With these mods, schema/notifications.js
+// file is set up to normalize notifications data, and there is a utility
+// function to get notifications by user ID.
+
+import{ schema } from 'normalizr';
+import * as notificationsData from '../../notifications.json';
 
 const user = new schema.Entity("users");
-const message = new schema.Entity("messages", {
-  idAttribute: (entity) => entity.guid
+const message = new schema.Entity("messages", {}, {
+  idAttribute: 'guid' // use guid as the unique identifier for messages instead of default id
 });
 const notification = new schema.Entity("notifications", {
   author: user,
   context: message
 });
-const normalizedData = normalize(notificationsData, [notification]);
+export { user, message, notification };
 
 /**
  * Get all notifications by user ID.
