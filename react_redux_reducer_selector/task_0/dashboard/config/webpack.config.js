cc95062355b71a2e2043781ee750386edae7fbe9
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-05-24 14:36:56 -0500

    react reducer selector directory and task 0, no tests yet

---
+++ react_redux_reducer_selector/task_0/dashboard/config/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // 'development' or 'production'
+  entry: path.resolve(__dirname, '../src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devServer: {
+    static: path.resolve(__dirname, '../dist'),
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+};
