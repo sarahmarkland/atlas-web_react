c6a87eb070c5b3c970a3254fa52235ebab59a7f2
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-05-24 15:32:39 -0500

    tests

--- react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
+++ react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
@@ -7,12 +7,16 @@ import {
     LOGOUT,
 } from '../actions/uiActionTypes';
 
+// Define an object with three properties; this intial state provides 
+// default values that the application can rely on when it first starts.
 const initialState = {
     isNotificationDrawerVisible: false,
     isUserLoggedIn: false,
     user: {},
 };
 
+// For each action type, the reducer returns a new state object with the
+// updated values.
 const uiReducer = (state = initialState, action) => {
     switch (action.type) {
         case DISPLAY_NOTIFICATION_DRAWER:
