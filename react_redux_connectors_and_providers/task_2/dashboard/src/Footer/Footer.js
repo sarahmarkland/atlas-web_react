702a84f2968be9034129f6709ee6cc9017eb07d8
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-06-05 13:09:01 -0500

    task 9

--- react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,23 +1,23 @@
-import React, { useContext } from 'react';
+import react from 'react';
+import { connect } from 'react-redux';
 import './Footer.css';
 import { getFooterCopy, getFullYear } from '../utils/utils';
-import AppContext from '../App/AppContext';
 
-function Footer() {
-  const { user } = useContext(AppContext);
-
+function Footer({ user }) {
   return (
-    <>
-      <footer className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      {user.isLoggedIn && (
-        <p>
-          <a href="#contact" id="contact-link">Contact us</a>
-        </p>
-      )}
-      </footer>
-    </>
+    <footer className="App-footer">
+      <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+    {user.isLoggedIn && (
+      <p>
+        <a href="#contact" id="contact-link">Contact us</a>
+      </p>
+    )}
+    </footer>
   );
 }
 
-export default Footer;
+const mapStateToProps = (state) => ({
+  user: state.uiReducer.get('user').toJS(),
+});
+
+export default connect(mapStateToProps)(Footer);
